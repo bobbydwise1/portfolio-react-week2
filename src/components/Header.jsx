b545f9f7d31e488526ea31c56de7cb811e90e77e
Robert Lee <bobbydwise1@hotmail.com>
2019-02-15 14:28:29 -0800

    Adjust header menus slightly.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <div style={compBox}>
       <h1>Tamerlame 2000 Chess</h1>
-      <Link to="/">Home</Link> <Link to="/rules">Rules</Link> <Link to="/pieces">Game Pieces</Link> <Link to="/signin">Sign In</Link> <Link to="/signout">Sign Out</Link>
+      <Link to="/">Home</Link> <Link to="/gameroute">Game</Link> <Link to="/pieces">Pieces</Link> <Link to="/signin">Sign In</Link> <Link to="/signout">Sign Out</Link>
     </div>
   );
 }
