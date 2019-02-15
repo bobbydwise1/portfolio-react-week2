c7331db3ad1191e1664e5dfb28224c006d76e47b
Robert Lee <bobbydwise1@hotmail.com>
2019-02-15 08:43:34 -0800

    Add additional base files again.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Header(){
   return (
     <div>
-      <h1>&#x265A; &#x265A; &#x265A; &#x265A; &#x265A; Help Queue &#x265A; &#x265A; &#x265A; &#x265A; &#x265A;</h1>
-      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+      <h1>&#x265A; &#x265A; &#x265A; &#x265A; &#x265A; CHESS VARIANT &#x265A; &#x265A; &#x265A; &#x265A; &#x265A;</h1>
+      <Link to="/">Home</Link> <Link to="/someother">Rules</Link> <Link to="/pieces">Game Pieces</Link>
     </div>
   );
 }
