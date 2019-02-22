dc02f80c1903f0d27eb0489b65a4d5f84b4a159b
Robert Lee <bobbydwise1@hotmail.com>
2019-02-22 09:25:13 -0800

    Add more images.

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import MySampleImage from '../assets/images/testimage.png';
+import MaxwellsEquations from '../assets/images/MaxwellsEquations.gif';
+import LightningFree from '../assets/images/LightningFree.jpg';
+import DigitalMultiMeter from '../assets/images/DigitalMultiMeter.jpg';
 
 function Welcome(){
   const compBox = {
@@ -10,7 +13,7 @@ function Welcome(){
 
   const imgBoxParent = {
     display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr"
+    gridTemplateColumns: "1fr 1fr"
   }
 
   const imgBox = {
@@ -25,21 +28,43 @@ function Welcome(){
     margin: "auto"
   }
 
+  const imgMakeFit = {
+    maxWidth: "100%",
+    maxHeight: "100%",
+    height: "100%",
+    margin: "auto",
+    padding: "auto"
+  }
+
+  const imgMakeHalfTall = {
+    maxWidth: "100%",
+    maxHeight: "50%",
+    margin: "auto",
+    padding: "auto"
+  }
+
   return (
+    <div>
     <div style={compBox}>
       <p>I'm currently in the process of making a career change into software development.  My professional backround is Electrical Engineering.</p>
       <div style={imgBoxParent}>
         <div style={imgBox}>
-          <img src={MySampleImage} style={mainImageStyle}/>
+          <img src={LightningFree} style={imgMakeFit}/>
         </div>
         <div style={imgBox}>
-          <img src={MySampleImage} style={mainImageStyle}/>
+          <img src={DigitalMultiMeter} style={imgMakeHalfTall}/>
+        </div>
+      </div>
+      <div style={imgBoxParent}>
+        <div style={imgBox}>
+          <img src={MaxwellsEquations} style={imgMakeFit}/>
         </div>
         <div style={imgBox}>
-          <img src={MySampleImage} style={mainImageStyle}/>
+          <img src={MaxwellsEquations} style={imgMakeFit}/>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
