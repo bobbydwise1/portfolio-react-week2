4eb353d83f97a16fa46f3e8dddc03b77f35aaedd
Robert Lee <bobbydwise1@hotmail.com>
2019-02-22 13:54:41 -0800

    Create save point before Exp component refactor.

---
+++ src/components/Resume.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+function Resume(){
+  const compBox = {
+    border: "solid grey",
+    color: "green",
+    backgroundColor: "black"
+  }
+
+  const styleBold = {
+    fontWeight: "bold",
+    fontSize: "14px"
+  }
+
+  const styleItalicBold = {
+    fontWeight: "bold",
+    fontStyle: "italic"
+  }
+
+  const styleItalic = {
+    fontStyle: "italic"
+  }
+
+  const styleNormal = {
+    fontStyle: "normal"
+  }
+
+  const centerText = {
+    textAlign: "center",
+    paddingBottom: "10px",
+    textShadow: "2px 2px 8px #448844"
+  }
+
+  const leftText = {
+    textAlign: "left",
+    paddingBottom: "10px",
+    textShadow: "2px 2px 8px #448844"
+  }
+
+  return ( <div>
+    <ul><p>Skills:</p>
+    <li>Ruby</li>
+    <li>Ruby on Rails</li>
+    <li>React</li>
+    <li>Redux</li>
+    <li>Angular</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+    <li>Babel</li>
+    <li>jQuery</li>
+    <li>Git</li>
+    <li>Github.com</li>
+    <li>Node.js Package Manager</li>
+    <li>Atom</li>
+    <li>Karma</li>
+    <li>RegEx</li>
+    <li>SQL</li>
+    <li>Firebase</li>
+    <li>AutoCAD</li>
+    <li>AutoCAD Electrical</li>
+    <li>SPICE</li>
+    <li>SIMATIC PLC</li>
+    <li>SIMATIC HMI</li>
+    <li>STEP7</li>
+    <li>WINCC</li>
+    <li>DIGSI</li>
+    <li>Senior Project Engineer</li>
+    <li>Industrial Control Engineer</li>
+    <li>Electrical Engineer</li>
+    <li>Debugging</li>
+    <li>Technical Writing</li>
+    <li>Technical Reading</li>
+    <li>Electrical Schematics Creation</li>
+    <li>2D Graphics rendering</li>
+    <li>AutoCAD Expert</li>
+    <li>Test Procedure Creation</li>
+    <li>Test Procedure Execution</li>
+    <li>Technical Instructor</li>
+    <li>Military Veteran</li>
+    <li>High Voltage Electrical Safety</li>
+    <li>Product Commissioning</li>
+    <li>Customer Technical Support</li>
+
+    <h4>Education and Training:</h4>
+
+    Epicodus, Portland, Oregon, Oct 2018-April 2019:
+    Learned computer programming, web development, software testing, software version control using Git, object oriented programming, functional programming, API’s, web app deployment, bug fixing, and programming best practices.
+
+    B.S. Electrical Engineering, Portland State University, Portland, Oregon, 2004-2010:
+    Software packages and languages studied: MATLAB, C++, C, Verilog, SPICE, MathCAD, Mathematica, Allen-Bradley PLC.  Courses included Linear Algebra and Thermodynamics.
+
+    Primary Leadership Development Course, Non-Commissioned Officer’s Academy, US Army, Fort Drum, NY, Aug 2003:
+    First Line Supervisor Leadership school.
+
+    Electrical Technician Training, US Army Ordinance School, Huntsville, AL, Apr 2000 - Oct 2000:
+    	Electrical repair and maintenance training.
+
+    Career History and Accomplishments:
+
+    Senior Project Engineer,  SIEMENS Industry, Inc., Mobility Division, Turnkey Projects and Electrification Office, Tualatin, OR, 2011- 2018:
+    DC Electrical Substation design.
+    Meet external customers nationally.
+    Review technical specifications.
+    Hardware and software integration.
+    PLC programming.
+    UI/UX design for Industrial Control.
+    Engineering Process Improvement
+    AutoCAD, AutoCAD Electrical
+    Industrial Controls Design
+    Industrial Controls Testing
+    Project Product Commissioning
+    Senior Project Electrical Engineer
+
+    	Project Engineer in Global Fortune 500 company, for Power Substation products for the Light Rail industry.  Promoted to Senior Project Engineer.  Worked in projects all over continental US, and Hawaii.  Review customer specification documents and meet customer requirements. Work with technology team to deliver project’s products.  Use AutoCAD Electrical to create smart electrical industrial control schematics. Write product testing procedures.  Conduct product testing.  Continually conduct process improvement.  Commission engineering products.  Answer engineering, technical, and product questions from the customer.  Some projects included Washington DC Streetcar, San Jose Light Rail, and Cincinnati Streetcar.
+
+    Team Leader, Oregon National Guard, Portland, OR, 2004- 2005
+    Shop Operations
+    Shop Maintenance
+    Supervisor
+    Training Instructor
+    Secret Security Clearance
+    Honorably discharged
+
+    Shop Manager, US Army, 10th Mountain Division, Fort Drum, NY, 2003 - 2004:
+    Electronics Repair
+    Shop Operations
+    Shop Maintenance
+    Supervisor
+    Rapid Promotion
+    Training Instructor
+    Secret Security Clearance
+    Honorably discharged
+
+    Electronics Technician, US Army, 1st Infantry Division, City of Kitzingen, State of Bavaria, Germany, 2000-2003:
+    Electronics Repair
+    Shop Maintenance
+    Rapid Promotion
+    Secret Security Clearance
+
+    Humanitarian Volunteer, Amigos de las Americas, Portland, OR, and Central America, 1997 - 1998:
+    Humanitarian Volunteer
+    Housing Construction
+    Public Health Instructor
+
+    Other Accomplishments:
+    Engineering in Training (E.I.T.) rating, April 2011.
+    Can solve 3x3x3 Rubik’s cube and 4x4x4 Rubik’s cube.
+
+
+
+
+</div>  )
+
+}
